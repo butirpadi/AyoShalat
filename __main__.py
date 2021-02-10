87adb128aefb4868b229d9e2742f05b5f86649d4
butirpadi <butirpadi@gmail.com>
2021-02-10 08:20:05 +0700

    [UPDATE] open window on center of screen

--- __main__.py
+++ __main__.py
@@ -4,6 +4,7 @@ from PySide6.QtWidgets import QApplication
 from ayoshalat import AyoShalat
 import os
 import pathlib
+from pprint import pprint
 
 if __name__ == "__main__":
     app = QApplication([])
@@ -14,6 +15,13 @@ if __name__ == "__main__":
     icopath = current_directory + '/icon/masjid.xpm'
     window.setWindowIcon(QIcon(icopath))
 
+    # set window in center of screen
+    # window.setGeometry(app.primaryScreen().geometry().getRect().)
+    screen = app.primaryScreen().geometry().getRect()
+    win = window.geometry().getRect()
+    window.setGeometry((screen[2]/2)-(win[2]/2),(screen[3]/2)-(win[3]/2),0,0)
+    
+    
     window.show()
     window.runningme()
     app.exec_()
