04ea787f76baa5ef31dcbd5ac885d9c441e77ed2
butirpadi <butipadi@gmail.com>
2021-03-20 07:41:26 +0700

    [UPDATE] disable pyglet

--- azanplay.py
+++ azanplay.py
@@ -1,10 +1,10 @@
-import pyglet, multiprocessing
+import multiprocessing
 from playsound import playsound
 
 class AzanPlay(object):
     def __init__(self, filepath):
         self.filepath = filepath
-        self.azansnd = pyglet.media.load(filepath)
+        # self.azansnd = pyglet.media.load(filepath)
         self.player = multiprocessing.Process(target=playsound, args=(filepath,))
 
     def stop(self):
